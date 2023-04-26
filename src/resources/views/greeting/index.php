5e16af8270c59c9df654b617cf2a3907917abbfc
ukaAzu <yukan1988@gmail.com>
2023-04-27 05:21:40 +0900

    edit form

--- src/resources/views/greeting/index.php
+++ src/resources/views/greeting/index.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <h1>Greeting</h1>
-        <form action="greeting/welcome" method="post">
+        <form action="/greeting/welcome" method="post">
             <?php echo e(csrf_field()); ?>
             <input type="text" name="username">
             <input type="submit" value="送信">
